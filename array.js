6163f547edddd3ffa9a1584592a2435cedc013ba
veerankipavan1998 <101305692+veerankipavan1998@users.noreply.github.com>
2022-04-05 09:12:22 +0530

    Create array.js

---
+++ array.js
@@ -0,0 +1,16 @@
+var data;
+data = 20;
+var abc;
+abc = 10;
+abc = " helo0";
+var foodItem;
+foodItem = ["pizza", " sandwich", 8];
+foodItem.push("briyani");
+foodItem.pop();
+//tuple  -> like array, but fixed elements
+// var myArrayCheck = ["paneer",8,true];
+// var myArrayCheck : String[];
+var myArrayCheck;
+myArrayCheck = [3, false];
+myArrayCheck = [3, " fasooos"];
+myArrayCheck = [1];

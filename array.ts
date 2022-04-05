bf419894d1f29187b934e3c62959e449d5528e1b
veerankipavan1998 <101305692+veerankipavan1998@users.noreply.github.com>
2022-04-05 09:12:58 +0530

    Create array.ts

---
+++ array.ts
@@ -0,0 +1,21 @@
+var data : number;
+data = 20;
+
+var abc: number;
+abc = 10;
+abc = " helo0";
+
+var foodItem : String[];
+foodItem= ["pizza"," sandwich",8];
+
+foodItem.push("briyani");
+foodItem.pop();
+
+//tuple  -> like array, but fixed elements
+
+// var myArrayCheck = ["paneer",8,true];
+// var myArrayCheck : String[];
+var myArrayCheck : [number,boolean];
+myArrayCheck = [3,false];
+myArrayCheck = [3," fasooos"];
+myArrayCheck=[1];
